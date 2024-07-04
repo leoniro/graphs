4d4c8a955c0811499b6dedc39424ce07ca3ffaf7
Leo <leoniro@gmail.com>
2024-07-03 21:40:09 -0300

    added key

--- Graph.py
+++ Graph.py
@@ -1,5 +1,6 @@
 class Graph():
     def __init__(self):
+        self.keys = []
         self.matrix = []
         self.size = 0
 
@@ -13,28 +14,64 @@ class Graph():
         col =  [row[j] for row in self.matrix]
         return [(i, wt) for i, wt in enumerate(col) if wt is not None]
     
-    def addVertex(self, n = 1):
-        for i in range(0,n):
-            [[ row.append(None) for row in self.matrix ]]
-            self.size += 1
-            self.matrix.append([None] * self.size)
+    def addVertex(self, key):
+        [[ row.append(None) for row in self.matrix ]]
+        self.size += 1
+        self.matrix.append([None] * self.size)
+        self.keys.append(key)
         return self.size
     
-    def removeVertex(self, i):
+    def removeVertex(self, key):
+        try:
+            i = self.keys.index(key)
+        except:
+            print("chave não existe")
+            return None
         [_.pop(i) for _ in self.matrix ]
         self.matrix.pop(i)
         return None
     
-    def addEdge(self, i, j, wt):
+    def addEdge(self, k1, k2, wt):
+        try:
+            i = self.keys.index(k1)
+            j = self.keys.index(k2)
+        except:
+            print("alguma das chaves não existe")
+            return None
         self.matrix[i][j] = wt
         return None
 
-    def removeEdge(self, i, j):
+    def removeEdge(self, k1, k2):
+        try:
+            i = self.keys.index(k1)
+            j = self.keys.index(k2)
+        except:
+            print("alguma das chaves não existe")
+            return None
         self.matrix[i][j] = None
         return None
 
-    def getWeight(self, i,j):
+    def getWeight(self, k1, k2):
+        try:
+            i = self.keys.index(k1)
+            j = self.keys.index(k2)
+        except:
+            print("alguma das chaves não existe")
+            return None
         return self.matrix[i][j]
+
+def dfs(g: Graph, x, i = 0):
+    n = i
+    visited = [n]
+    stack = g.edgesFrom(n)
+    while len(stack) > 0:
+        n = stack.pop()
+
+
+
+def bfs(g, x, i = 0):
+    visited = [i]
+    queue = []
 
 
 x = Graph()
